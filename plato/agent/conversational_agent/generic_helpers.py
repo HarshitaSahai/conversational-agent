b65b9e73cceb1401e7fdd2415361d5f455afbda4
Harshita Sahai <harshitasahai24@gmail.com>
2020-07-15 17:20:16 +0530

    first commit

---
+++ plato/agent/conversational_agent/generic_helpers.py
@@ -0,0 +1,96 @@
+"""
+Copyright (c) 2019 Uber Technologies, Inc.
+
+Licensed under the Uber Non-Commercial License (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at the root directory of this project.
+
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+__author__ = "Alexandros Papangelis"
+
+from plato.agent.component.conversational_module import ConversationalModule
+from gtts import gTTS
+
+import os
+
+
+class GenericTextInputHelper(ConversationalModule):
+    """
+    This class is a helper for listening to text input for Generic agents.
+    """
+
+    def __init__(self, args):
+        super(GenericTextInputHelper, self).__init__()
+
+    def initialize(self, args):
+        pass
+
+    def receive_input(self, args):
+        pass
+
+    def generate_output(self, args=None):
+
+        # Listen for input
+        utterance = input('USER > ')
+
+        return utterance
+
+    def train(self, dialogue_episodes):
+        pass
+
+    def load(self, path):
+        pass
+
+    def save(self):
+        pass
+
+
+class GenericSpeechSynthesiserHelper(ConversationalModule):
+    """
+    This class is a helper for listening to text input for Generic agents.
+    """
+
+    def __init__(self, args):
+        super(GenericSpeechSynthesiserHelper, self).__init__()
+
+    def initialize(self, args):
+        pass
+
+    def receive_input(self, args):
+        pass
+
+    def generate_output(self, args=None):
+
+        utterance = ''
+        if 'utterance' in args:
+            utterance = args['utterance']
+
+        elif 'args' in args and isinstance(args['args'], str):
+            utterance = args['args']
+
+        # Synthesise speech
+        try:
+            tts = gTTS(utterance)
+            tts.save('sys_output.mp3')
+            os.system('afplay sys_output.mp3')
+
+        except Exception as e:
+            print(
+                'WARNING: gTTS encountered an error: {0}. '
+                'Falling back to System TTS.'.format(e)
+            )
+            os.system('say ' + utterance)
+
+        return utterance
+
+    def train(self, dialogue_episodes):
+        pass
+
+    def load(self, path):
+        pass
+
+    def save(self):
+        pass
